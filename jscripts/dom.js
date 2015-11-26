5d89f4caded25965ed840fed12d1295454fa9009
MD4472 <dnlp200@gmail.com>
2015-11-26 15:34:08 -0500

    Add updated files with working modals

--- jscripts/dom.js
+++ jscripts/dom.js
@@ -5,10 +5,42 @@ $(document).ready(function(){
   $(".btn-primary").on("click", function(preventSubmit){
   preventSubmit.preventDefault();
 
-    $("#divLivingston").removeClass('hidden');
-    $("#divNewark").addClass('hidden');
 
-  });
+    // $("#divL").removeClass('hidden');
+    $("#divN").addClass('hidden');
+    $("#divL").addClass('hidden');
+    $("#divNB").addClass('hidden');
+
   
 
+    var city = $("#text1").val().trim();
+    console.log(city);
+    // var city2 = city.toLowerCase();
+    // console.log(city2);
+    //var isDuplicate = false;
+    // $("td.word-td").each(function() {
+    //   if($(this).text().trim().toLowerCase() === newWord.toLowerCase()) {
+    //     isDuplicate = true;
+    //     return;
+//    if (condition1) {
+//     block of code to be executed if condition1 is true
+// } else if (condition2) {
+//     block of code to be executed if the condition1 is false and condition2 is true
+// } else {
+//     block of code to be executed if the condition1 is false and condition2 is false
+// } 
+    if (city == "Newark") {
+      $("#divN").removeClass('hidden');
+    }
+
+    else if (city == "Livingston") {
+      $("#divL").removeClass('hidden');
+    }
+    
+    else if (city == "New Brunswick") {
+       $("#divNB").removeClass('hidden');
+    }
+
 });
+});
+
