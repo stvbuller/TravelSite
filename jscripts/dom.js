fd5473d5548c8f0935567f482ae5a29e981fb945
Steven Buller <steven@Stevens-MacBook-Pro.local>
2015-11-26 12:42:04 -0500

    added modals

--- jscripts/dom.js
+++ jscripts/dom.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 
+ 
+  //event listener for ".btn-info"
+  $(".btn-primary").on("click", function(preventSubmit){
+  preventSubmit.preventDefault();
+
+    $("#divLivingston").removeClass('hidden');
+    $("#divNewark").addClass('hidden');
+
+  });
+  
 
 });
