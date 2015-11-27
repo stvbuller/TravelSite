5f67f588eb9090730b1be79e6989a962d814c88b
Steven Buller <steven@Stevens-MacBook-Pro.local>
2015-11-27 15:26:48 -0500

    changed information in alert

--- jscripts/dom.js
+++ jscripts/dom.js
@@ -20,14 +20,9 @@ $(document).ready(function(){
     console.log(checkout);
     console.log(city);
 
-<<<<<<< HEAD
-    if (checkin === "") {
-      alert("Hello");
-=======
     <!--forces user to enter in city, checkin and checkout-->
     if (city == "" || checkin == "" || checkout == "") {
-      alert("Please enter a city, a check in date and a check out date.");
->>>>>>> 4fc4e12407b3ae12d276801ec360e4d0a7a5c77a
+      alert("Please enter Newark, Livingston, or New Brunswick, a check in date and a check out date.")
     }
 
     <!--when entering city remove class of hidden respective div-->
