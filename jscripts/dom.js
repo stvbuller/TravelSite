7a1aed0d83cfdea6feaf71d401cd4b4ffd24036c
Richenda Gould <Richenda Gould>
2016-01-08 20:35:26 -0500

    Assignment changes

--- jscripts/dom.js
+++ jscripts/dom.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
- 
   //event listener for ".btn-info"
   $(".btn-primary").on("click", function(preventSubmit){
   preventSubmit.preventDefault();
@@ -23,25 +22,18 @@ $(document).ready(function(){
     console.log(city);
 
     //forces user to enter in city, checkin and checkout//
-    if (city === "New Brunswick" || city === "Newark" || city === "Livingston") {
+    if (city === "New Brunswick" || city === "Newark" || city === "Livingston"){
       validCity = true;
     }
 
-    if (checkin != "" && checkout != "") {
+    if (checkin != "" && checkout != ""){
       validTimeIn = true, validTimeOut = true;}
-    
 
     if (validCity == false || validTimeIn == false || validTimeOut == false) {
       //alert("Please enter Newark, Livingston, or New Brunswick, a check in date and a check out date.");
-      $('#modalInputWarning').modal('show') 
+      $('#modalInputWarning').modal('show')
        return;
     }
-
-
-     
-     
-
-
 
     //when entering city remove class of hidden respective div//
     if (city == "Newark") {
@@ -51,14 +43,13 @@ $(document).ready(function(){
     else if (city == "Livingston") {
       $("#divL").removeClass('hidden');
     }
-    
+
     else if (city == "New Brunswick") {
        $("#divNB").removeClass('hidden');
     }
 
-  
   });
-  
+
   //the "remove listing" button on the modal hides the listing
   $("#removedivN").on("click", function(){
     $("#divN").addClass('hidden');
@@ -68,24 +59,10 @@ $(document).ready(function(){
   $("#removedivL").on("click", function(){
     $("#divL").addClass('hidden');
   });
-  
+
   //the "remove listing" button on the modal hides the listing
   $("#removedivNB").on("click", function(){
     $("#divNB").addClass('hidden');
   });
 
-    // $('#am1').on('click', function() {
-    //     $('#am0').text('Free Breakfast');
-    //   });
-      
-    // $('#am2').on('click', function() {
-    //     $('#am0').text('Free Wifi');
-    //   });
-
-    // $('#am3').on('click', function() {
-    //     $('#am0').text('Pool'); 
-    //   });
-
     });
-
-
